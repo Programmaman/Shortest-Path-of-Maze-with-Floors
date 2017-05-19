9bd7df4ac7d68ff625186e1a1859188739377170
Programmaman <alexbraynen@hotmail.com>
2017-05-18 23:58:57 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,130 +1,278 @@
+#include <cstdio>
 #include <iostream>
-#include <cmath>
-#include <climits>
-#include <string>
+#include <vector>
 #include <fstream>
+#include <string>
+#include <list>
+#include <utility>
+#include <queue>
+#include <deque>
+
 using namespace std;
 
+//Tile Structure
+typedef struct  {
+	int TileID;
+	bool visited;
+	vector <int> NeighborsID;
+}Tile;
 
-
-int GetValue(int ** values, int * arr, int a, int b)
+//Output Overload for testing
+ostream& operator << (ostream &os, const Tile& t)
 {
-	if (values[a][b] != 0)
-		return values[a][b];
-
-	int value = 0;
-	for (int i = a; i <= b; i++)
+	os << t.TileID << ' ' << boolalpha << t.visited << "Edge IDs:";
+	for ( int l = 0; l < t.NeighborsID.size(); l++)
 	{
-		value = value + arr[i];
+		os << t.NeighborsID.at(l) << ' ';
 	}
-
-	values[a][b] = value;
-	return value;
+	os << endl;
+		return os;
 }
 
-int GetInequality(int ** values, int * arr, int t, int a, int b)
+//Write Path function
+void writePath(vector<Tile> patharray, const int rootid, const int goalid, const int numofcolumns, const int numofrows, ofstream &outputfile)
 {
-	return (int)pow((t - GetValue(values, arr, a, b)), 2);
-}
-
-void Reconstruct(int * splits, int n, int a, string &stg, int & splitcount)
-{
-	splitcount++;
-	stg += to_string(splits[a] - a + 1) + " ";
-	if (splits[a] == n - 1)
-		return;
-	Reconstruct(splits, n, splits[a] + 1,stg,splitcount);
-}
-
-int Algorithm(int * arr, const int n, const int t, ofstream &outputfile)
-{
-	int *inequalities, *splits, **values;
-	inequalities = new int[n]();
-	splits = new int[n]();
-	
-	values = new int*[n];
-	for (int i = 0; i < n; i++)
-		values[i] = new int[n]();
-
-	for (int i = n-1; i >= 0; i--)
+	vector <char> output;
+	for (int i = patharray.size()-1; i >= 0;)
 	{
-		if (i == n-1)
+		
+		bool neighborfound = false;
+		Tile tempnode = patharray.at(i);
+		int gotoidx = i;
+		for (int j = 0; j < tempnode.NeighborsID.size(); j++)
 		{
-			inequalities[i] = GetInequality(values, arr, t, i, i);
-			splits[i] = n-1;
+			for (int k = i; k >= 0; k--)
+			{
+				if (tempnode.NeighborsID[j] == patharray[k].TileID)
+				{
+					gotoidx = k;
+					neighborfound = true;
+
+					// Print direction of next node
+					if (patharray[k].TileID == tempnode.TileID + 1)
+					{
+						//outputfile << "W ";
+						output.push_back('W');
+					}
+					else if (patharray[k].TileID == tempnode.TileID - 1)
+					{
+						//outputfile << "E ";
+						output.push_back('E');
+					}
+					else if (patharray[k].TileID == tempnode.TileID - numofcolumns)
+					{
+						//outputfile << "S ";
+						output.push_back('S');
+					}
+					else if (patharray[k].TileID == tempnode.TileID + numofrows)
+					{
+						//outputfile << "N ";
+						output.push_back('N');
+					}
+					else if (patharray[k].TileID == tempnode.TileID + numofrows * numofcolumns)
+					{
+						//outputfile << "D ";
+						output.push_back('D');
+					}
+					else if (patharray[k].TileID == tempnode.TileID - numofrows * numofcolumns)
+					{
+						//outputfile << "U ";
+						output.push_back('U');
+					}
+				}
+
+			}
+		}
+		if (neighborfound == true)
+		{
+			i = gotoidx;
 		}
 		else
 		{
-			int min = INT_MAX;
-			int split = n-1;
+			i--;
+		}
 
-			//when j = n-1 this means dont split at all actually
-			for (int j = i; j <= n-1; j++)
-			{
-				if (GetValue(values, arr, i, j) > t)
-					break;
-				int inequality = GetInequality(values, arr, t, i, j) + (j < n-1 ? inequalities[j + 1] : 0); //0 = no split
-				if (inequality < min)
-				{
-					min = inequality;
-					split = j;
-				}
-			}
-			inequalities[i] = min;
-			splits[i] = split;
+	}
+	for (int iterate = output.size()-1; iterate >=0; iterate --)
+	{
+		outputfile << output[iterate];
+		outputfile << ' ';
+		if (iterate == 0)
+		{
+			break;
 		}
 	}
-	
 
-
-	string stg;
-	int splitcount = 0;
-
-
-
-
-	Reconstruct(splits, n, 0, stg,splitcount);
-	outputfile << splitcount << " " << stg << endl;
-	
-
-	return 0;
+	outputfile << endl << endl;
 }
 
+//BFS 
+void BFS(vector<Tile> tiles, vector<Tile> &patharray, const int rootid, const int goalid)
+{
+	Tile tempnode;
+	tiles[rootid].visited = true;
+	queue <Tile> queueoftiles;
+	queueoftiles.push(tiles[rootid]);
+
+	while (queueoftiles.size() != 0)
+	{
+		tempnode = queueoftiles.front();
+		queueoftiles.pop();
+		bool flag = false;
+
+		for ( int second_iterator = 0; second_iterator < tempnode.NeighborsID.size(); second_iterator++)
+		{
+			if (tiles[tempnode.NeighborsID[second_iterator]].visited != true)
+			{
+				patharray.push_back(tiles[tempnode.TileID]);
+				break;
+			}
+			
+		}
+		
 
 
+		for ( int iterator = 0; iterator < tempnode.NeighborsID.size(); iterator++)
+		{
+			if (tiles[tempnode.NeighborsID[iterator]].visited == true)
+			{
+				;
+			}
+			else
+			{
 
+				tiles[tempnode.NeighborsID[iterator]].visited = true;
+				queueoftiles.push(tiles[tempnode.NeighborsID[iterator]]);
+			}
 
+		}
+		
+	}
+	patharray.push_back(tiles[goalid]);
+}
 
-
+//Main
 int main()
 {
-	cout << "Running algorithm" << endl;
-	int p;
-	int n;
-	int limit;
+	int floornum;
+	int rownum;
+	int columnnum;
+	int numMazes;
 
-	ifstream infilename;
-	ofstream outfilename;
-	infilename.open("input.txt");
-	outfilename.open("output.txt");
-	if (!infilename.is_open())
+	cout << "Running Algorithm" << endl;
+	
+	//Read File
+	ifstream infile("input.txt");
+	ofstream outfile("output.txt");
+	if (!infile.is_open())
 	{
-		cout << "File not found.";
+		cout << "Error opening input file." << endl;
 		return 0;
 	}
-	infilename >> p;
-	for (int i = 1; i <= p; i++)
+	else if (!outfile.is_open())
 	{
-		infilename >> n;
-		infilename >> limit;
-		int *arr;
-		arr = new int[n];
-		for (int j = 0; j < n; j++)
+		cout << "Error opening output file." << endl;
+		return 0;
+	}
+	infile >> numMazes;
+	for (int i = 1; i <= numMazes; i++)
+	{
+
+		int numTiles;
+		int numoffloors, numofrows, numofcolumns;
+		int rootID;
+		int goalID;
+
+		infile >> numoffloors;
+		infile >> numofrows;
+		infile >> numofcolumns;
+		numTiles = numoffloors * numofrows * numofcolumns;
+		vector <Tile> arrayoftiles(numTiles);
+		vector <Tile> arrayofpaths;
+		infile >> floornum;
+		infile >> rownum;
+		infile >> columnnum;
+
+		rootID = (floornum* numofrows * numofcolumns) + (rownum * numofcolumns) + columnnum;
+
+		infile >> floornum;
+		infile >> rownum;
+		infile >> columnnum;
+
+
+
+		goalID = (floornum* numofrows * numofcolumns) + (rownum * numofcolumns) + columnnum;
+		
+		// Assign neighbors to Tiles
+		for (int j = 0; j < numTiles; j++)
 		{
-			infilename >> arr[j];
+			string availablepaths;
+
+
+			arrayoftiles[j].TileID = j;
+			arrayoftiles[j].visited = false;
+
+			infile >> availablepaths;
+
+			if (availablepaths[0] == '0')
+			{
+				;
+			}
+			else if (availablepaths[0] == '1')
+			{
+				arrayoftiles[j].NeighborsID.push_back(j - numofcolumns);
+			}
+			if (availablepaths[1] == '0')
+			{
+				;
+			}
+			else if (availablepaths[1] == '1')
+			{
+				arrayoftiles[j].NeighborsID.push_back(j + 1);
+			}
+			if (availablepaths[2] == '0')
+			{
+				;
+			}
+			else if (availablepaths[2] == '1')
+			{
+				arrayoftiles[j].NeighborsID.push_back(j + numofcolumns);
+			}
+			if (availablepaths[3] == '0')
+			{
+				;
+			}
+			else if (availablepaths[3] == '1')
+			{
+				arrayoftiles[j].NeighborsID.push_back(j - 1);
+			}
+			if (availablepaths[4] == '0')
+			{
+				;
+			}
+			else if (availablepaths[4] == '1')
+			{
+				arrayoftiles[j].NeighborsID.push_back(j + numofrows*numofcolumns);
+			}
+			if (availablepaths[5] == '0')
+			{
+				;
+			}
+			else if (availablepaths[5] == '1')
+			{
+				arrayoftiles[j].NeighborsID.push_back(j - numofrows*numofcolumns);
+			}
+			
 		}
-		Algorithm(arr, n, limit,outfilename);
+		BFS(arrayoftiles, arrayofpaths, rootID, goalID);
+		writePath(arrayofpaths, rootID, goalID, numofcolumns, numofrows, outfile);
+		
+
+
 	}
 	cout << "'output.txt' file created." << endl;
+	cout << "Please exit this window. And open the Outputfile.";
 	cin.get();
-}
+	return 0;
+	}
+	
